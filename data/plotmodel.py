b7877bd45ad05aa55603fcf880f070ee8a36b8b3
gronki <gronki@gmail.com>
2017-10-25 01:32:03 +0200

    skrypt do rysowania poprawiony do nowego kodu

--- data/plotmodel.py
+++ data/plotmodel.py
@@ -11,6 +11,7 @@ m2i = lambda m: 10 ** ((5.5 - m) / 2.5)
 clevels = [
     17.000,
     18.000,
+    18.500,
     19.000,
     19.500,
     20.000,
@@ -30,7 +31,8 @@ clevels = [
 ccolors = [
     '#FAF4D7',
     '#DED226',
-    '#F49136',
+    '#FCB370',
+    '#ED8627',
     '#B65950',
     '#D96228',
     '#949935',
@@ -55,6 +57,8 @@ for fn in argv[1:]:
     nx = gd.RasterXSize
     ny = gd.RasterYSize
     arr = gd.ReadAsArray()
+    hobs = arr[0,:,:]
+    sky = arr[1:,:,:]
 
     fs = ( 8 * np.sqrt(nx/float(ny)), 6 / np.sqrt(nx/float(ny)) )
 
@@ -65,30 +69,47 @@ for fn in argv[1:]:
     x = np.linspace(gt[0], gt[0] + nx * gt[1], nx)
     y = np.linspace(gt[3], gt[3] + ny * gt[5], ny)
 
+    #---------------------------------------------------------------------------
+
     fig,ax = plt.subplots(figsize = fs)
-    m = ax.imshow(arr[2,:,:], cmap = 'BrBG_r')
+    m = ax.imshow(hobs, cmap = 'BrBG_r')
     ax.set_aspect('auto')
 
     plt.colorbar(m, ax = ax)
     plt.savefig(sub(r'\.tif$','.el.png',fn), dpi = 144, interpolation = 'none')
 
+    #---------------------------------------------------------------------------
+
     fig,ax = plt.subplots(figsize = fs)
-    m = ax.contourf(x, y, i2m(arr[1,:,:] + m2i(22)), levels = clevels, colors = ccolors)
+    m = ax.contourf(x, y, i2m(sky[3,:,:]), levels = clevels, colors = ccolors)
     plt.colorbar(m, ax = ax)
     ax.set_aspect('auto')
 
     plt.savefig(sub(r'\.tif$','.png',fn), dpi = 144)
 
+    #---------------------------------------------------------------------------
+
     fig,ax = plt.subplots(figsize = fs)
-    m = ax.contourf(x, y, i2m(arr[0,:,:] + m2i(22)), levels = clevels, colors = ccolors)
+    m = ax.contourf(x, y, i2m(sky[2,:,:]), levels = clevels, colors = ccolors)
     ax.set_aspect(1 / np.cos(51.0 * np.pi / 180))
     ax.set_aspect('auto')
     plt.colorbar(m, ax = ax)
     plt.savefig(sub(r'\.tif$','.noat.png',fn), dpi = 144)
 
-    fig,ax = plt.subplots(figsize = fs)
-    m = ax.imshow(-2.5 * np.log10((arr[1,:,:] + m2i(22)) / (arr[0,:,:] + m2i(22))), cmap = 'gray_r', vmin = 0, vmax = 0.1, interpolation = 'none')
-    ax.set_aspect('auto')
+    #---------------------------------------------------------------------------
 
+    fig,ax = plt.subplots(figsize = fs)
+    m = ax.imshow(-2.5 * np.log10(sky[3,:,:] / sky[2,:,:]), cmap = 'inferno_r',  vmin = 0.0, vmax = 0.1, interpolation = 'none')
+    ax.set_aspect('auto')
     plt.colorbar(m, ax = ax)
     plt.savefig(sub(r'\.tif$','.atdiff.png',fn), dpi = 144)
+
+    #---------------------------------------------------------------------------
+
+    fig,ax = plt.subplots(figsize = fs)
+    m = ax.imshow(-2.5 * np.log10(sky[2,:,:] / sky[1,:,:]), cmap = 'RdGy',  vmin = -0.1, vmax = 0.1, interpolation = 'none')
+    ax.set_aspect('auto')
+    plt.colorbar(m, ax = ax)
+    plt.savefig(sub(r'\.tif$','.phdiff.png',fn), dpi = 144)
+
+    #---------------------------------------------------------------------------
