5ae9f2d667756519680fb449380bbc0d77e6f192
gronki <gronki@gmail.com>
2017-10-27 18:15:45 +0200

    na razie ostateczna wersja rysowania

--- data/plotmodel.py
+++ data/plotmodel.py
@@ -8,45 +8,39 @@ from re import match, sub
 i2m = lambda i: 5.5 - 2.5 * np.log10(i)
 m2i = lambda m: 10 ** ((5.5 - m) / 2.5)
 
-clevels = [
-    17.000,
-    18.000,
-    18.500,
-    19.000,
-    19.500,
-    20.000,
-    20.500,
-    20.750,
-    21.000,
-    21.125,
-    21.250,
-    21.375,
-    21.500,
-    21.550,
-    21.600,
-    21.650,
-    21.700,
+colors = [
+    (18.000, '#FFFFFF'),
+    (19.400, '#D2613D'),
+    (20.000, '#FFDE00'),
+    (20.800, '#476100'),
+    (21.100, '#2E98A6'),
+    (21.450, '#0E0036'),
+    (21.600, '#3A0047'),
+    (21.700, '#1C1C1C'),
+    (22.000, '#000000'),
 ]
 
-ccolors = [
-    '#FAF4D7',
-    '#DED226',
-    '#FCB370',
-    '#ED8627',
-    '#B65950',
-    '#D96228',
-    '#949935',
-    '#628A29',
-    '#1FA364',
-    '#15ACA3',
-    '#1E737C',
-    '#0C4654',
-    '#1E3542',
-    '#0B2737',
-    '#062012',
-    '#1A0F28',
-    '#0C0A0E',
-]
+# ccolors = [ c for l,c in colors ]
+# clevels = [ l for l,c in colors ]
+
+sg = { 'red': [], 'green': [], 'blue': [] }
+for l,c in colors:
+    cr = int(c[1:3], 16)
+    cg = int(c[3:5], 16)
+    cb = int(c[5:7], 16)
+    sg['red'  ].append(( (l - 18.0) / (22.0 - 18.0), cr / 255.0, cr / 255.0))
+    sg['green'].append(( (l - 18.0) / (22.0 - 18.0), cg / 255.0, cg / 255.0))
+    sg['blue' ].append(( (l - 18.0) / (22.0 - 18.0), cb / 255.0, cb / 255.0))
+
+clevels = np.concatenate([
+    np.arange(18.0, 20.0, 0.250),
+    np.arange(20.0, 20.8, 0.125),
+    np.arange(20.8, 21.5, 0.050),
+    np.arange(21.5, 21.8, 0.025),
+    np.array([22.0]) ])
+
+from matplotlib.colors import LinearSegmentedColormap
+cmap = LinearSegmentedColormap('sky', sg)
 
 for fn in argv[1:]:
 
@@ -60,7 +54,7 @@ for fn in argv[1:]:
     hobs = arr[0,:,:]
     sky = arr[1:,:,:]
 
-    fs = ( 8 * np.sqrt(nx/float(ny)), 6 / np.sqrt(nx/float(ny)) )
+    fs = ( 10 * np.sqrt(nx/float(ny)), 10 / np.sqrt(nx/float(ny)) )
 
     ex = [ gt[0], gt[0] + nx * gt[1],
            gt[3] + ny * gt[5], gt[3] ]
@@ -81,7 +75,7 @@ for fn in argv[1:]:
     #---------------------------------------------------------------------------
 
     fig,ax = plt.subplots(figsize = fs)
-    m = ax.contourf(x, y, i2m(sky[3,:,:]), levels = clevels, colors = ccolors)
+    m = ax.contourf(x, y, i2m(sky[3,:,:]), levels = clevels, cmap = cmap)
     plt.colorbar(m, ax = ax)
     ax.set_aspect('auto')
 
@@ -90,7 +84,8 @@ for fn in argv[1:]:
     #---------------------------------------------------------------------------
 
     fig,ax = plt.subplots(figsize = fs)
-    m = ax.contourf(x, y, i2m(sky[2,:,:]), levels = clevels, colors = ccolors)
+    m = ax.contourf(x, y, i2m(sky[2,:,:]), levels = clevels, cmap = cmap)
+    # m = ax.imshow(i2m(sky[2,:,:]), cmap = cmap, vmin = 18.0, vmax = 22.0)
     ax.set_aspect(1 / np.cos(51.0 * np.pi / 180))
     ax.set_aspect('auto')
     plt.colorbar(m, ax = ax)
@@ -111,5 +106,3 @@ for fn in argv[1:]:
     ax.set_aspect('auto')
     plt.colorbar(m, ax = ax)
     plt.savefig(sub(r'\.tif$','.phdiff.png',fn), dpi = 144)
-
-    #---------------------------------------------------------------------------
